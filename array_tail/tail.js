dff735e400d5183bacef6618e1e2fda56e2277e0
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-13 10:35:46 +0100

    javascript init

--- array_tail/tail.js
+++ array_tail/tail.js
@@ -5,14 +5,21 @@ Chiediamo all'utente quanti elementi dell'array dovremo stampare
 Suggerimenti:
 Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array */
 
-let userNumbers =[parseInt(prompt("Quanti numeri vuoi inserire?"))] ;
+let numbers = [];
+let userNumbers = parseInt(prompt("Quanti numeri vuoi inserire?"));
 console.log (userNumbers);
-let outputNumbers = Math.floor(Math.random() * (1 - 100 + 1) ) + 1;
-console.log(outputNumbers);
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-/* let elements = (userNumbers = outputNumbers); */
 
-/* for(i = 100; i >= outputNumbers.lenght; i--); */
+
+let randomNumbers = getRndInteger(1, 100);
+console.log (randomNumbers);
+
+/* /* for (let i = 1; t=100; i<t) i++; { */
+    /* randomNumbers= userNumbers.push (Math.round(Math.random() * t));
+} */
 
 
 
